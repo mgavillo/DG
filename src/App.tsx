6843c6bfc99ab5a7b69a9b3841ba5ea833fb07c7
mgavillo <marie.gavillon@gmail.com>
2023-02-15 22:33:29 +0000

    react-mapbox first draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Maps from "./Maps";
 import Timeline from "./Timeline";
-
 function App(){
   return(
-    <div className="App">
+    <div className="App flex justify-start items-start flex-col p-8">
+      <h1 className="pb-12 text-4xl">Absurbary Farm</h1>
+      <Maps />
       <Timeline/>
     </div>
   )
